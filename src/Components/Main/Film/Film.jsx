b7b2cdfee0671872626ff13522db91bd5d17c17d
Aleksandr Heletukha <41300278+aleksnadrtw@users.noreply.github.com>
2020-04-09 12:51:30 +0300

    Minor update,fix layout, begin build component Page

--- src/Components/Main/Film/Film.jsx
+++ src/Components/Main/Film/Film.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Film.module.css'
 import Block from '../Block/Block'
+import {NavLink} from "react-router-dom";
 
 let filmData = [
     {name: 'Iron Man', poster: 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'},
@@ -9,9 +10,13 @@ let filmData = [
 
 const Film = (props) => {
     return (
-        <div className={style.inner}>
-            <Block name={filmData[0].name} poster={filmData[0].poster}/>
-            <Block name={filmData[1].name} poster={filmData[1].poster}/>
+        <div className="inner">
+            <NavLink to="">
+                <Block name={filmData[0].name} poster={filmData[0].poster}/>
+            </NavLink>
+            <NavLink to="">
+                <Block name={filmData[1].name} poster={filmData[1].poster}/>
+            </NavLink>
         </div>
     );
 };
