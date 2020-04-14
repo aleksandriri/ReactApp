f2abd066a2d3e352c77a559a69ab4b0a3dd370be
Aleksandr Heletukha <41300278+aleksnadrtw@users.noreply.github.com>
2020-04-14 13:57:59 +0300

    Refactor StateRedux.jsx

--- src/Redux/StateRedux.jsx
+++ src/Redux/StateRedux.jsx
@@ -1,73 +1,70 @@
-let rerenderEntireTree = () => {
-    console.log('State change');
-};
 let key = '57ed170a';
 let link = `http://www.omdbapi.com/?apikey=${key}&s=guns`;
 console.log(link);
 
-let stateRedux = {
-    FilmData:[
-        {
-            name: 'Iron Man',
-            poster: 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'
-        },
-        {
-            name: 'Iron Fists',
-            poster: 'https://m.media-amazon.com/images/M/MV5BMTg5ODI3ODkzOV5BMl5BanBnXkFtZTcwMTQxNjUwOA@@._V1_SX300.jpg'
-        },
-        {
-            name: '2 Guns',
-            poster: 'https://m.media-amazon.com/images/M/MV5BNTQ5MTgzNDg4OF5BMl5BanBnXkFtZTcwMjAyODEzOQ@@._V1_SX300.jpg'
-        },
-        {
-            name: 'Avatar',
-            poster: 'https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg'
-        }
-    ],
-    SerialData:[
-        {
-            name: 'Friends',
-            poster: 'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'
-        },
-        {
-            name: 'El Narco',
-            poster: 'https://m.media-amazon.com/images/M/MV5BOThkNTRhZTctMWMwNy00MDdlLWI1MWEtOGJhNjAwMzQzMTEwXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
-        },
-        {
-            name: 'Money Heist',
-            poster: 'https://m.media-amazon.com/images/M/MV5BOTI5OTI0YTQtM2UxNC00MjMxLWE5NjQtZWIzNGRhZTlmMjdhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg'
-        },
-        {
-            name: 'The Money Pit',
-            poster: 'https://m.media-amazon.com/images/M/MV5BNDJmNTZkMDMtOWU5ZS00N2ZlLWIxN2EtZTI4NzBhODc0MmRjL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'
-        }
-    ],
-    newSearchText: ''
+let store = {
+    _stateRedux : {
+        FilmData:[
+            {
+                name: 'Iron Man',
+                poster: 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'
+            },
+            {
+                name: 'Iron Fists',
+                poster: 'https://m.media-amazon.com/images/M/MV5BMTg5ODI3ODkzOV5BMl5BanBnXkFtZTcwMTQxNjUwOA@@._V1_SX300.jpg'
+            },
+            {
+                name: '2 Guns',
+                poster: 'https://m.media-amazon.com/images/M/MV5BNTQ5MTgzNDg4OF5BMl5BanBnXkFtZTcwMjAyODEzOQ@@._V1_SX300.jpg'
+            },
+            {
+                name: 'Avatar',
+                poster: 'https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg'
+            }
+        ],
+        SerialData:[
+            {
+                name: 'Friends',
+                poster: 'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'
+            },
+            {
+                name: 'El Narco',
+                poster: 'https://m.media-amazon.com/images/M/MV5BOThkNTRhZTctMWMwNy00MDdlLWI1MWEtOGJhNjAwMzQzMTEwXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg'
+            },
+            {
+                name: 'Money Heist',
+                poster: 'https://m.media-amazon.com/images/M/MV5BOTI5OTI0YTQtM2UxNC00MjMxLWE5NjQtZWIzNGRhZTlmMjdhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg'
+            },
+            {
+                name: 'The Money Pit',
+                poster: 'https://m.media-amazon.com/images/M/MV5BNDJmNTZkMDMtOWU5ZS00N2ZlLWIxN2EtZTI4NzBhODc0MmRjL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'
+            }
+        ],
+        newSearchText: ''
+    },
+    getState () {
+        return this._stateRedux;
+    },
+    _callSubscriber () {
+        console.log('State change');
+    },
+    addBlock () {
+        let newBlock = {
+            name: this._stateRedux.newSearchText,
+            poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'
+        };
+        this._stateRedux.FilmData.push(newBlock);
+        this._callSubscriber(this._stateRedux);
+    },
+    updateNewSearchText (newText) {
+        this._stateRedux.newSearchText = newText;
+        this._callSubscriber(this._stateRedux);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }
 };
 
-window.stateRedux = stateRedux;
+window.store = store;
 
-export const addBlock = () => {
-    let newBlock = {
-        name: stateRedux.newSearchText,
-        poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'
-    };
-    let validate = () => {
-        if (stateRedux.newSearchText.length > 1){
-            stateRedux.FilmData.push(newBlock);
-        }
-    };
-    validate();
-    rerenderEntireTree(stateRedux);
-};
-
-export const updateNewSearchText = (newText) => {
-    stateRedux.newSearchText = newText;
-    rerenderEntireTree(stateRedux);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-};
-
-export default stateRedux;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import stateRedux, {subscribe} from "./Redux/StateRedux";
-import {addBlock, updateNewSearchText} from "./Redux/StateRedux";
+import store from "./Redux/StateRedux";
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (stateRedux) =>{
     ReactDOM.render(
         <BrowserRouter>
-            <App state={stateRedux} addBlock={addBlock} updateSearch={updateNewSearchText}/>
+            <App state={store.getState()} addBlock={store.addBlock.bind(store)} updateSearch={store.updateNewSearchText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root')
     );
 };
 
-rerenderEntireTree(stateRedux);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
