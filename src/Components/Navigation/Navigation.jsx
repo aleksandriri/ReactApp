b589480d99a6d755f0b5e6ca4ac11e24791bb72e
Aleksandr Heletukha <41300278+aleksnadrtw@users.noreply.github.com>
2020-05-02 17:51:33 +0300

    Refactor

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import style from './Navigation.module.css'
 import {NavLink} from "react-router-dom";
-import {HomeFilled, SettingFilled, FolderFilled} from '@ant-design/icons';
+import logo from '../../img/logo.svg';
+import {
+    HomeFilled,
+    SettingFilled,
+    FolderFilled,
+    FacebookFilled,
+    LinkedinFilled,
+    TwitterSquareFilled,
+    CustomerServiceFilled,
+    PhoneFilled
+} from '@ant-design/icons';
 
 const Navigation = () => {
     return (
         <div className={style.navigation}>
             <div className={style.navigation__inner}>
                 <div className={style.navigation__logo}>
-                    <img src="https://web-creator.ru/uploads/Page/25/react.svg" alt="React"/>
+                    <a href="#">
+                        <img src={logo} alt="Logo"/>
+                    </a>
                 </div>
                 <nav className={style.nav}>
-                    <NavLink className={style.nav__link} to="/film" activeClassName={style.active}><HomeFilled />Фильмы</NavLink>
+                    <NavLink className={style.nav__link} to="/home" activeClassName={style.active}><HomeFilled />Главная</NavLink>
+                    <NavLink className={style.nav__link} to="/film" activeClassName={style.active}><FolderFilled />Фильмы</NavLink>
                     <NavLink className={style.nav__link} to="/serial" activeClassName={style.active}><FolderFilled />Сериалы</NavLink>
                     <NavLink className={style.nav__link} to="/cartoon" activeClassName={style.active}><FolderFilled />Мультфильмы</NavLink>
                     <NavLink className={style.nav__link} to="/settings" activeClassName={style.active}><SettingFilled />Настройки</NavLink>
                 </nav>
+                <div className={style.navigation__social}>
+                    <p className={style.navigation__social_title}>Social media:</p>
+                    <div className={style.navigation__social_wrapper}>
+                        <a href="https://www.facebook.com" target="_blank"><FacebookFilled /></a>
+                        <a href="https://www.linkedin.com/" target="_blank"><LinkedinFilled /></a>
+                        <a href="https://twitter.com" target="_blank"><TwitterSquareFilled /></a>
+                    </div>
+                </div>
             </div>
             <div className={style.navigation__footer}>
-
+                <div className={style.navigation__footer_inner}>
+                    <div className={style.navigation__footer_header}>
+                        <CustomerServiceFilled />
+                    </div>
+                    <div className={style.navigation__footer_body}>
+                        <p className={style.navigation__footer_body_title}>Customer Support</p>
+                        <a className={style.navigation__footer_body_link} href="tel:+303-339-0949"><PhoneFilled />303-339-0949</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
