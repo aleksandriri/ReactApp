35ffdfd66fe13a28e35840fcd07963a222694eb6
Aleksandr Heletukha <heletukha@gmail.com>
2020-06-29 15:46:21 +0300

    Updates

---
+++ precache-manifest.a201b522726b954f317b0bd851076050.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255f17d6ca259316c26471d9cbae99e4",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "a6feb367fca695ac357d",
+    "url": "/ReactApp/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "ecf76766aba50004a4e2",
+    "url": "/ReactApp/static/css/main.8ab0df14.chunk.css"
+  },
+  {
+    "revision": "a6feb367fca695ac357d",
+    "url": "/ReactApp/static/js/2.0d881414.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/ReactApp/static/js/2.0d881414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf76766aba50004a4e2",
+    "url": "/ReactApp/static/js/main.b6b8f8cb.chunk.js"
+  },
+  {
+    "revision": "00e682f4b90eb97b09c9",
+    "url": "/ReactApp/static/js/runtime-main.1ea43ea0.js"
+  },
+  {
+    "revision": "a568cd56b334084b985944afa987cc71",
+    "url": "/ReactApp/static/media/logo.a568cd56.svg"
+  },
+  {
+    "revision": "4591684f418788678eb9f522574a380d",
+    "url": "/ReactApp/static/media/slide-1.4591684f.png"
+  }
+]);
